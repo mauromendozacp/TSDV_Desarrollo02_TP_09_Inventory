7cabf32486f4958723edff3787894eb5e117cfb4
Mauro <mauro.mendozacp@gmail.com>
2021-06-12 02:42:22 -0300

    player open inventory

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,26 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
+
+[Serializable]
 public class PlayerMesh
 {
+    public Mesh hair;
     public Mesh armor;
     public Mesh gloves;
     public Mesh boots;
 }
-
 public class Player : Character
 {
-    [SerializeField] private GameObject[] playerMesh = new GameObject[7];
-    [SerializeField] private GameObject hairMesh;
+    [SerializeField] private GameObject[] playerMesh = new GameObject[8];
+    [SerializeField] private GameObject[] playerUIMesh = new GameObject[8];
     [SerializeField] private PlayerMesh playerDefaultMesh;
-
-    Equipment equipment;
-    Inventory inventory;
-    Animator anim;
-
-    public delegate void RefreshMesh();
-    public static RefreshMesh OnRefreshMeshAsStatic;
+    [SerializeField] private GameObject inventoryPanel;
 
     public enum PlayerPart
     {
@@ -34,22 +29,31 @@ public class Player : Character
         Arms
     }
 
+    Equipment equipment;
+    Inventory inventory;
+    Animator anim;
+
+    public delegate void RefreshMesh();
+    public static RefreshMesh OnRefreshMeshAsStatic;
+
+    
+
     private void Awake()
     {
-        // equipment = GetComponent<Equipment>();
-        // inventory = GetComponent<Inventory>();
+        equipment = GetComponent<Equipment>();
+        inventory = GetComponent<Inventory>();
         anim = GetComponent<Animator>();
     }
 
     private void Start()
     {
-       // GameplayManager.GetInstance().SetPlayer(this);
-       // OnRefreshMeshAsStatic += UpdateMesh;
+       GameplayManager.GetInstance().SetPlayer(this);
+       OnRefreshMeshAsStatic += UpdateMesh;
     }
 
     void OnDestroy()
     {
-       // OnRefreshMeshAsStatic -= UpdateMesh;
+       OnRefreshMeshAsStatic -= UpdateMesh;
     }
 
     public List<Slot> GetSaveSlots()
@@ -87,15 +91,81 @@ public class Player : Character
         inventory.SetNewInventory(itemsList);
     }
 
+    bool IsMoving()
+    {
+        return Mathf.Abs(Input.GetAxis("Horizontal")) > 0f || Mathf.Abs(Input.GetAxis("Vertical")) > 0f;
+    }
+
+    void UpdateMoveAnimation()
+    {
+        float speed = (Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical")));
+        anim.SetFloat("Speed", speed);
+    }
+
+    void PickUp()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("PickUp");
+        }
+    }
+
+    void OpenInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (!inventoryPanel.activeSelf)
+            {
+                inventoryPanel.SetActive(true);
+                UpdatePlayerUi();
+            }
+            else
+            {
+                inventoryPanel.SetActive(false);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (IsMoving() && !anim.GetCurrentAnimatorStateInfo(0).IsName("PickUp"))
+        {
+            SetMovement(direction, GetDirection());
+            direction = GetDirection();
+            UpdateMoveAnimation();
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0f);
+        }
+        PickUp();
+        OpenInventory();
+    }
+
     public void UpdateMesh()
     {
-       SetMesh(0, equipment.GetEquipmentList()[4].ID, PlayerPart.Helmet);
-       SetMesh(1, equipment.GetEquipmentList()[5].ID, PlayerPart.Gloves);
-       SetMesh(2, equipment.GetEquipmentList()[6].ID, PlayerPart.Boots);
-       SetMesh(3, equipment.GetEquipmentList()[7].ID, PlayerPart.Shoulders);
-       SetMesh(4, equipment.GetEquipmentList()[8].ID, PlayerPart.Armor);
-       SetMesh(5, equipment.GetEquipmentList()[0].ID, PlayerPart.Arms);
-       SetMesh(6, equipment.GetEquipmentList()[1].ID, PlayerPart.Arms);
+        SetMesh(0, equipment.GetEquipmentList()[4].ID, PlayerPart.Helmet);
+        SetMesh(2, equipment.GetEquipmentList()[7].ID, PlayerPart.Shoulders);
+        SetMesh(3, equipment.GetEquipmentList()[8].ID, PlayerPart.Armor);
+        SetMesh(4, equipment.GetEquipmentList()[5].ID, PlayerPart.Gloves);
+        SetMesh(5, equipment.GetEquipmentList()[6].ID, PlayerPart.Boots);
+        SetMesh(6, equipment.GetEquipmentList()[0].ID, PlayerPart.Arms);
+        SetMesh(7, equipment.GetEquipmentList()[1].ID, PlayerPart.Arms);
+
+        UpdatePlayerUi();
+    }
+
+    void UpdatePlayerUi()
+    {
+        if (inventoryPanel.activeSelf)
+        {
+            for (int i = 0; i < playerMesh.Length - 2; i++)
+            {
+                playerUIMesh[i].GetComponent<SkinnedMeshRenderer>().sharedMesh = playerMesh[i].GetComponent<SkinnedMeshRenderer>().sharedMesh;
+                playerUIMesh[i].transform.localPosition = playerMesh[i].transform.localPosition;
+                playerUIMesh[i].transform.localEulerAngles = playerMesh[i].transform.localEulerAngles;
+            }
+        }
     }
 
     public void SetMesh(int index, int id, PlayerPart part)
@@ -106,8 +176,7 @@ public class Player : Character
             {
                 playerMesh[index].GetComponent<SkinnedMeshRenderer>().sharedMesh =
                     GameplayManager.GetInstance().GetItemFromID(id).mesh;
-                playerMesh[index].SetActive(true);
-                hairMesh.SetActive(false);
+                playerMesh[1].GetComponent<SkinnedMeshRenderer>().sharedMesh = new Mesh();
             }
             if (part == PlayerPart.Arms)
             {
@@ -117,11 +186,11 @@ public class Player : Character
                 if (index == 5)
                 {
                     playerMesh[index].transform.localPosition =
-                        ((Arms) (GameplayManager.GetInstance().GetItemFromID(id))).spawnPositionL.pos;
+                        ((Arms)(GameplayManager.GetInstance().GetItemFromID(id))).spawnPositionL.pos;
                     playerMesh[index].transform.localEulerAngles =
                         ((Arms)(GameplayManager.GetInstance().GetItemFromID(id))).spawnPositionL.rot;
                 }
-                else if(index == 6)
+                else if (index == 6)
                 {
                     playerMesh[index].transform.localPosition =
                         ((Arms)(GameplayManager.GetInstance().GetItemFromID(id))).spawnPositionR.pos;
@@ -141,8 +210,7 @@ public class Player : Character
             {
                 case PlayerPart.Helmet:
                     playerMesh[index].GetComponent<SkinnedMeshRenderer>().sharedMesh = new Mesh();
-                    playerMesh[index].SetActive(false);
-                    hairMesh.SetActive(true);
+                    playerMesh[1].GetComponent<SkinnedMeshRenderer>().sharedMesh = playerDefaultMesh.hair;
 
                     break;
                 case PlayerPart.Shoulders:
@@ -169,39 +237,5 @@ public class Player : Character
                     throw new ArgumentOutOfRangeException(nameof(part), part, null);
             }
         }
-    }
-
-    bool IsMoving()
-    {
-        return Mathf.Abs(Input.GetAxis("Horizontal")) > 0f || Mathf.Abs(Input.GetAxis("Vertical")) > 0f;
-    }
-
-    void UpdateMoveAnimation()
-    {
-        float speed = (Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical")));
-        anim.SetFloat("Speed", speed);
-    }
-
-    void PickUp()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            anim.SetTrigger("PickUp");
-        }
-    }
-
-    void Update()
-    {
-        if (IsMoving() && !anim.GetCurrentAnimatorStateInfo(0).IsName("PickUp"))
-        {
-            SetMovement(direction, GetDirection());
-            direction = GetDirection();
-            UpdateMoveAnimation();
-        }
-        else
-        {
-            anim.SetFloat("Speed", 0f);
-        }
-        PickUp();
     }
 }
