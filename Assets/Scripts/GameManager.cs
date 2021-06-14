ef5570a07804dadff767b4dfb6bea1b02b8933bb
Mauro <mauro.mendozacp@gmail.com>
2021-06-13 22:09:12 -0300

    main menu and game manager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int MainMenuScene { get; } = 0;
+    public int GamePlayScene { get; } = 1;
+    public int DeathScene { get; } = 2;
+    public int WinScene { get; } = 3;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public enum SceneGame
+    {
+        MainMenu,
+        GamePlay,
+        DeathScene,
+        WinScene
+    }
+
+    public SceneGame Scene { get; set; }
+    public bool Win { get; set; } = false;
+    public bool GameOver { get; set; }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void RestartGame()
+    {
+        GameOver = false;
+        Win = false;
+    }
+}
---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void PlayGame(string sceneName)
+    {
+        GameManager.Instance.ChangeScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
